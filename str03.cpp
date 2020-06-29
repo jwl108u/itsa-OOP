55a0808fd567abd2dbecb0911c34ebd1b3928d8e
jwl108u <59867984+jwl108u@users.noreply.github.com>
2020-06-29 12:42:58 +0800

    Create str03.cpp

---
+++ str03.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  
+using namespace std;  
+  
+int main() {  
+    // [C_ST03-©ö] ¸U°ê½XÂà¦¨¹ïÀ³¦r¤¸  
+    int ASCII;  
+    while(cin >> ASCII)  
+    {  
+        cout << char(ASCII) << endl;  
+    }  
+    return 0;  
+}
