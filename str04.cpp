682d38a4e4d38333bb26411a9be9fa8d6129d172
jwl108u <59867984+jwl108u@users.noreply.github.com>
2020-06-29 12:43:55 +0800

    Create str04.cpp

---
+++ str04.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  
+using namespace std;  
+   
+int main() {  
+    // [C_ST04-易] 字元符號轉換成標準萬國碼  
+    char input;  
+    while(cin >> input)  
+    {  
+        cout << int(input) << endl;  
+    }  
+    return 0;  
+}
